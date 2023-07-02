66d6f35452c567e0773fbad73e90729143f6b996
SgitBusity <72297937+AllianceTing@users.noreply.github.com>
2023-07-02 18:20:37 +0800

    feat: add a component to support rate limt control (#29)

---
+++ server/internal/middleware/frequency_control.go
@@ -0,0 +1,56 @@
+package middleware
+
+import (
+	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// define rate limit struct
+type frequencyControlByTokenBucket struct {
+	refreshRate float64    // 令牌的刷新速率
+	capacity    int64      // bucket's capacity
+	tokens      float64    // tokens' count
+	lastToken   time.Time  //latest time token stored
+	mtx         sync.Mutex // mutex
+}
+
+// allow frequency
+func (tb *frequencyControlByTokenBucket) Allow() bool {
+	tb.mtx.Lock()
+	defer tb.mtx.Unlock()
+	now := time.Now()
+	// compute tokens which needs
+	tb.tokens = tb.tokens + tb.refreshRate*now.Sub(tb.lastToken).Seconds()
+	if tb.tokens > float64(tb.capacity) {
+		tb.tokens = float64(tb.capacity)
+	}
+	// judge weather to pass through
+	if tb.tokens >= 1 {
+		tb.tokens--
+		tb.lastToken = now
+		return true
+	}
+	
+	return false
+}
+
+// LimitHandler registried a middle ware to use
+func LimitHandler(maxConn int, refreshRate float64) gin.HandlerFunc {
+	tb := &frequencyControlByTokenBucket{
+		capacity:    int64(maxConn),
+		refreshRate: refreshRate,
+		tokens:      0,
+		lastToken:   time.Now(),
+	}
+	return func(c *gin.Context) {
+		if !tb.Allow() {
+			c.String(503, "Too many request")
+			c.Abort()
+			
+			return
+		}
+		c.Next()
+	}
+}
