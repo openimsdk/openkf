c1985703023b1c3eff5d05d321d898650fbe4870
Asklv <47499836+IRONICBo@users.noreply.github.com>
2023-08-23 10:16:49 +0800

    feat: LLM server start with fastchat multiprocess. (#168)

--- bot/main.py
+++ bot/main.py
@@ -12,3 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import utils.config as config
+import utils.log as log
+from utils import banner
+import constants.constants as constants
+
+logger = None
+
+
+def main():
+    # Init system
+    kf_config = config.KBConfig("config.yaml")
+    global logger
+    log.KFLog.init_logger(
+        kf_config.get_app_log_path(), constants.LOG_LEVEL_DEBUG
+        if kf_config.get_app_debug() else constants.LOG_LEVEL_INFO)
+    logger = log.KFLog.get_logger()
+    banner.kf_banner(kf_config.get_app_version(), kf_config.get_app_debug(),
+                     kf_config.get_app_log_path())
+
+
+if __name__ == "__main__":
+    main()
--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -12,5 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if __name__ == "__main__":
-    pass
+__all__ = [
+    "banner",
+    "config",
+    "log"
+]
